a32172b988908e8856e072d165c6935f2431d713
fernandokkang <110192117+fernandokkang@users.noreply.github.com>
2024-05-03 09:30:40 +0900

    Update CartController.java

--- src/main/java/com/example/todaysbook/controller/CartController.java
+++ src/main/java/com/example/todaysbook/controller/CartController.java
@@ -29,8 +29,6 @@ public class CartController {
 
     private final CartService cartService;
 
-
-
     @GetMapping("/list")
     public String showMyCartList(Model model) {
         // userId가 1인 사용자의 장바구니 목록 조회
@@ -47,7 +45,6 @@ public class CartController {
         return "cart/list"; // 템플릿 이름 리턴
     }
 
-
     @PostMapping("/add")
     public ResponseEntity<Long> addToCart(@RequestBody CartRequestDto requestDto) {
         // 강제로 userId를 1로 설정하여 테스트
@@ -62,7 +59,6 @@ public class CartController {
         }
     }
 
-
     // 서버 측 컨트롤러
     @PostMapping("/delete-unselected")
     public ResponseEntity<?> deleteSelectedCartItems(@RequestBody List<Long> selectedIds) {
@@ -74,8 +70,4 @@ public class CartController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("success", false));
         }
     }
-
 }
-
-
-
